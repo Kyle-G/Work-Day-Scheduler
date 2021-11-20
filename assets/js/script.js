7c21c51529f7b47bad8934d6ad1d93880b6a79df
Kyle <kylegreer93@gmail.com>
2021-11-20 05:03:00 -0500

    adding background colors based off time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,93 @@
 
+var textArr = [];
+var isBefore = dateFns.isBefore;
+var subHours = dateFns.subHours;
+var getHours = dateFns.getHours;
 
-
+window.onload = function () {
+    update();
+    setInterval(update, 1000);
+}
 
 var update = function () {
-
     var today = dateFns.format(
         new Date(),
-        'MMMM-Do-YYYY HH:mm:ss'
-      )
-      
+        'MMMM Do, YYYY HH:mm:ss'
+      )    
     $("#currentDay").text(today);
+    var result = getHours(new Date());
+
+    
+    console.log(result);
+    $('textarea').each(function(index) {
+
+        if(textArr[index].id + 9 <= 12) {
+            if (textArr[index].id + 9 > result ) {
+                $(this).parent().parent().attr("class", "row bg-secondary border-top");
+            } else if (textArr[index].id + 9 == result) {
+                $(this).parent().parent().attr("class", "row bg-danger border-top");
+            } else if (textArr[index].id + 9 < result) {
+                $(this).parent().parent().attr("class", "row bg-warning border-top");
+            }
+        }
+
+        if(textArr[index].id + 9 > 12) {
+            if (textArr[index].id + 9 - 12 < result) {
+                $(this).parent().parent().attr("class", "row bg-secondary border-top");
+            } else if (textArr[index].id + 9 -12 == result) {
+                $(this).parent().parent().attr("class", "row bg-danger border-top");
+            } else if (textArr[index].id + 9 -12 > result) {
+                $(this).parent().parent().attr("class", "row bg-warning border-top");
+            }
+        }
+    });
 };
 
-$(document).ready(function(){
-    update();
+
+
+function assignID() {
+    $('textArea').each(function(index) {
+        $(this).attr("data-id", index);
+        textArr.push({'id': index, 'text': ''});
+    });
+}
+
+function btnHandler(button) {
+     thisText =  $(button).parent().prev("div").find("textarea").val();
+     
+     indexVariable = $(button).parent().prev("div").find("textarea").attr("data-id");
+     thisTime = $(button).parent().prev().attr("data-time", indexVariable + 9); 
+     console.log(textArr, "this is textArr");
+     if(textArr != null) {
+        textArr[indexVariable]={'id': parseInt(indexVariable), 'text': thisText};
+     }
+     saveText();
+    // index++;
+}
+
+function loadStorageTexts() {
+    $('textArea').each(function(index) {
+         $(this).val(textArr[index].text);
+    });
+}
+
+function loadTexts() {
     
-    setInterval(update, 1000);
-});
+    var savedTexts = localStorage.getItem('texts');
+
+    if(!savedTexts) {
+        return false;
+    }
+
+    savedTexts = JSON.parse(savedTexts);
+    console.log(savedTexts, "this is savedTexts");
+    textArr = savedTexts;
+    loadStorageTexts();
+}
+
+function saveText() {
+    localStorage.setItem("texts", JSON.stringify(textArr));
+}
+
+assignID();
+loadTexts();
